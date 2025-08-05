50ec32b72b699f118adc4adef71acc665b76e94c
Nikhil Chauhan <chauhannikhil.email@gmail.com>
2025-08-05 10:38:42 +0530

    Update 11_data_segmentation.sql

--- scripts/11_data_segmentation.sql
+++ scripts/11_data_segmentation.sql
@@ -1,1 +1,93 @@
+/*===============================================================================
+ 11 - Data Segmentation
+===============================================================================
 
+Purpose:
+    - To segment products based on price ranges
+    - To segment customers based on lifespan and spending behavior
+
+Key Analyses:
+    - Product cost segmentation: Below 100, 100–500, 500–1000, Above 1000
+    - Customer segmentation:
+        > VIP     : Lifespan ≥ 12 months AND Spending > ₹5000
+        > Regular : Lifespan ≥ 12 months AND Spending ≤ ₹5000
+        > New     : Lifespan < 12 months
+
+SQL Functions Used:
+    - SUM(), MIN(), MAX(), COUNT()
+    - CASE WHEN, GROUP BY, ORDER BY
+    - TIMESTAMPDIFF(), CTE (Common Table Expression)
+
+Notes:
+    - Helps identify premium vs budget products
+    - Enables targeting strategies based on customer category
+    - Outputs count of products and customers in each segment
+*/
+
+
+
+/*
+Segment products into cost ranges and count how many products fall into each segment
+*/
+
+WITH product_segments AS 
+(
+SELECT 
+	product_key,
+    product_name,
+    cost,
+    CASE 
+		WHEN cost < 100 THEN 'Below 100'
+        WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+        WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+        ELSE 'Above 1000'
+	END AS cost_range
+FROM gold_dim_products
+)
+SELECT 
+	cost_range,
+    COUNT(product_key) AS total_products
+FROM product_segments 
+GROUP BY cost_range
+ORDER BY total_products DESC;
+
+
+
+
+/*
+Group customers into three segments based on their spending behaviour
+	- VIP : Customers with at least 12 months of history and spending more than 5000.
+    - Regular : Customers with at least 12 months of history but spending 5000 or less.
+    - New : Customers with a lifespan of less than 12 months.
+    And find the total number of customers by each group
+*/
+
+
+WITH customer_spending AS (
+    SELECT
+        c.customer_key,
+        SUM(f.sales_amount) AS total_spending,
+        MIN(order_date) AS first_order,
+        MAX(order_date) AS last_order,
+        TIMESTAMPDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan
+    FROM gold_fact_sales f
+    LEFT JOIN gold_dim_customers c ON f.customer_key = c.customer_key
+    GROUP BY c.customer_key
+)
+
+SELECT
+    customer_segment,
+    COUNT(customer_key) AS total_customers,
+	ROUND(COUNT(customer_key) * 100.0 / SUM(COUNT(customer_key)) OVER (), 2) AS percentage_share
+FROM (
+    SELECT
+        customer_key,
+        CASE 
+            WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+            WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+            ELSE 'New'
+        END AS customer_segment
+    FROM customer_spending
+) t
+GROUP BY customer_segment
+ORDER BY total_customers DESC;
